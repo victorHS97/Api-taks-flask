6b399346f86f5e70462622e9ecdbb9ccb853babd
Victor Souza <victor.souza37@outlook.com>
2024-11-09 20:22:24 -0300

    finaly testing api

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ tasks = []
 
 def test_create_task():
     new_task = {
-        "title": "Tarefa Teste",
+        "title": "Estudar Laravel",
         "description": "testando o create"
     }
     response = requests.post(f'{baseURL}/tasks', json=new_task)
@@ -31,3 +31,35 @@ def test_get_task():
     response = requests.get(f'{baseURL}/tasks/{task_id}')
     response_json = response.json()
     assert task_id == response_json['id']
+
+
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        payload = {
+            "completed": True,
+            "description": "Estudado",
+            "title": "Estudar Laravel"
+        }
+    response = requests.put(f'{baseURL}/tasks/{task_id}', json=payload)
+    response.status_code = 200
+    response_json = response.json()
+    assert "message" in response_json
+
+    # nova requisição para validar o PUT
+    response = requests.get(f'{baseURL}/tasks/{task_id}')
+    response_json = response.json()
+    assert response_json['title'] == payload['title']
+    assert response_json['description'] == payload['description']
+    assert response_json['completed'] == payload['completed']
+
+
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+    response = requests.delete(f'{baseURL}/tasks/{task_id}')
+    response.status_code == 200
+
+    # verificando a exclusão
+    response = requests.get(f'{baseURL}/tasks/{task_id}')
+    response.status_code == 400
